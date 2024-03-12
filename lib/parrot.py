d7812b6c377866c08f7df669b2dc827878011687
Teddy mwangi <teddy.maina@student.moringaschool.com>
2024-03-12 10:23:03 +0300

    done

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,12 @@
-def parrot():
-    pass
+def parrot(string="Squawk!"):
+    """
+    Accepts a string argument, prints it, and returns it.
+    
+    Parameters:
+        string (str): The string to be printed and returned. Defaults to "Squawk!" if not provided.
+        
+    Returns:
+        str: The input string.
+    """
+    print(string)
+    return string
